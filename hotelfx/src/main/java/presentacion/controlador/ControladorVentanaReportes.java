3381b31a0c91e4952ffd636304e75ceef05a2a25
Marioc93 <mario_c_081293@hotmail.com>
2020-06-25 20:17:30 -0300

    Reporte ocupacion

--- hotelfx/src/main/java/presentacion/controlador/ControladorVentanaReportes.java
+++ hotelfx/src/main/java/presentacion/controlador/ControladorVentanaReportes.java
@@ -7,6 +7,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import modelo.Cuarto;
+import persistencia.dao.mysql.DAOSQLFactory;
+import presentacion.reportes.ReporteOcupacion;
 
 public class ControladorVentanaReportes implements Initializable{
 	
@@ -18,22 +21,34 @@ public class ControladorVentanaReportes implements Initializable{
 	@FXML private Button btnGenerarReporte;
 	@FXML private DatePicker fechaDesde;
 	@FXML private DatePicker fechaHasta;
+	
+	private Cuarto cuarto;
+	private String reporte;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		this.cuarto = new Cuarto(new DAOSQLFactory());
 	}
 	
 	@FXML
 	public void deshabilitarParametrosFechas() {
 		fechaDesde.setDisable(true);
 		fechaHasta.setDisable(true);
+		this.reporte = "ocupacion";
 	}
 	
 	@FXML
 	public void habilitarParametrosFechas() {
 		fechaDesde.setDisable(false);
 		fechaHasta.setDisable(false);
+	}
+	
+	@FXML
+	public void generarReporteOcupacion() {
+		if(this.reporte.equals("ocupacion")) {
+			ReporteOcupacion reporteOcupacion = new ReporteOcupacion(cuarto.obtenerCuartos());
+			reporteOcupacion.mostrar();
+		}		
 	}
 
 }
