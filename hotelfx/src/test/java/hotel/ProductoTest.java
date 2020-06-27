cf2ebcf5fe977ab344a52cc41f9367351cdd0d31
Marioc93 <mario_c_081293@hotmail.com>
2020-06-27 00:44:25 -0300

    Agregado de tests sin funcar

---
+++ hotelfx/src/test/java/hotel/ProductoTest.java
@@ -0,0 +1,64 @@
+package hotel;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import dto.ProductoDTO;
+import modelo.Producto;
+import persistencia.dao.mysql.DAOSQLFactory;
+
+public class ProductoTest {
+	
+	private ProductoDTO productoDto;
+	private Producto producto;
+	
+	@Before
+	public void init() {
+		
+		producto = new Producto(new DAOSQLFactory());
+		productoDto = new ProductoDTO(0, "Pollo frito", new BigDecimal(520.00), "sale con fritas", "El pollo loco", "Comida");
+		producto.agregarProducto(productoDto);
+		
+	}
+	
+	@Test
+	public void testBuscarProductosProv() throws Exception {
+		producto = new Producto(new DAOSQLFactory());
+		List<ProductoDTO> listaProductos = producto.buscarProductos("comi");
+		System.out.println(listaProductos.size());
+		assertEquals(1, listaProductos.size());
+	
+	}
+	
+	@Test
+	public void testEditarProductos() throws Exception {
+		producto = new Producto(new DAOSQLFactory());
+		productoDto.setNombre("Mila frita");
+		producto.modificarProducto(productoDto);
+		System.out.println(productoDto.getNombre());
+		assertEquals("Mila frita", productoDto.getNombre());
+				
+	}
+	
+	@After
+	public void borrarProducto() {
+		producto = new Producto(new DAOSQLFactory());		
+		int ultimoIdProducto = producto.obtenerProductos().get(producto.obtenerProductos().size()-1).getIdProducto();
+		ProductoDTO ultimoProdAgregado = null;
+		
+		for(ProductoDTO p : producto.obtenerProductos()) {
+			if(p.getIdProducto() == ultimoIdProducto) {
+				ultimoProdAgregado = p;
+			}
+		}
+		
+		producto.borrarProducto(ultimoProdAgregado);
+	}
+
+}
